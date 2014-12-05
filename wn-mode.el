c3a0478bd985c8dec789b49ef828eaaeef6d4801
Luís Oliveira <luismbo@gmail.com>
2014-12-05 12:11:22 +0000

    Rev 4 from http://www.emacswiki.org/emacs/wn-mode.el

--- wn-mode.el
+++ wn-mode.el
@@ -1,3 +1,8 @@
+;;; wn-model.el -- numeric window switching shortcuts
+;;; Version: 0.1
+;;; 
+
+;;; Window number
 (defun wn-window-list ()
   "Return a list of ordered windows on the current frame"
   (window-list (selected-frame) 0 (minibuffer-window)))
@@ -14,61 +19,31 @@
 
 (defun wn-selected-window-number ()
   "Return the number of the selected window"
-  (length (memq (selected-window) (reverse (wn-window-list)))))
-
-;(defun wn-selected-window-modeline ()
-;  "Return the string for the current window modeline"
-;  (propertize (format " #%s" (wn-selected-window-number))
-;              'face 'wn-modeline-face))
+  (1- (length (memq (selected-window) (reverse (wn-window-list))))))
 
 (defun wn-selected-window-modeline ()
-  (format " #%s" (wn-selected-window-number)))
+  "Return the string for the current window modeline"
+  (propertize (format " #%s" (wn-selected-window-number))
+              'face 'wn-modeline-face))
 
-(defvar wn-mode-map nil
-  "Keymap.")
-
-(unless wn-mode-map
-  (setq wn-mode-map (make-sparse-keymap))
-        
-  (define-key wn-mode-map (kbd "M-1")
-    (lambda nil (interactive)
-      (wn-select-nth 0)))
-
-  (define-key wn-mode-map (kbd "M-2")
-    (lambda nil (interactive)
-      (wn-select-nth 1)))
-
-  (define-key wn-mode-map (kbd "M-3")
-    (lambda nil (interactive)
-      (wn-select-nth 2)))
-
-  (define-key wn-mode-map (kbd "M-4")
-    (lambda nil (interactive)
-      (wn-select-nth 3)))
-
-  (define-key wn-mode-map (kbd "M-5")
-    (lambda nil (interactive)
-      (wn-select-nth 4)))
-
-  (define-key wn-mode-map (kbd "M-6")
-    (lambda nil (interactive)
-      (wn-select-nth 5)))
-
-  (define-key wn-mode-map (kbd "M-7")
-    (lambda nil (interactive)
-      (wn-select-nth 6)))
-
-  (define-key wn-mode-map (kbd "M-8")
-    (lambda nil (interactive)
-      (wn-select-nth 7)))
-
-  (define-key wn-mode-map (kbd "M-9")
-    (lambda nil (interactive)
-      (wn-select-nth 8)))
-
-  (define-key wn-mode-map (kbd "M-0")
-    (lambda nil (interactive)
-      (wn-select-nth 9))))
+(defvar wn-mode-map
+  (if (boundp 'wn-mode-map)
+      wn-mode-map
+    (let ((map (make-sparse-keymap)))
+      (mapc (lambda (keydef) (define-key map (car keydef) (cadr keydef)))
+            '(("\C-c0" (lambda nil (interactive) (wn-select-nth 0)))
+              ("\C-c1" (lambda nil (interactive) (wn-select-nth 1)))
+              ("\C-c2" (lambda nil (interactive) (wn-select-nth 2)))
+              ("\C-c3" (lambda nil (interactive) (wn-select-nth 3)))
+              ("\C-c4" (lambda nil (interactive) (wn-select-nth 4)))
+              ("\C-c5" (lambda nil (interactive) (wn-select-nth 5)))
+              ("\C-c6" (lambda nil (interactive) (wn-select-nth 6)))
+              ("\C-c7" (lambda nil (interactive) (wn-select-nth 7)))
+              ("\C-c8" (lambda nil (interactive) (wn-select-nth 8)))
+              ("\C-c9" (lambda nil (interactive) (wn-select-nth 9)))
+              ("\C-c#" wn-select-nth)))
+      map))
+  "Wn-mode map")
 
 (define-minor-mode wn-mode
   "A minor mode that enables selection of windows according to
@@ -76,8 +51,12 @@
   :group 'windows
   :global t
   :init-value nil
-  :lighter (:eval (wn-selected-window-modeline)))
+  :lighter ( :eval (wn-selected-window-modeline)))
 
-;(defface wn-modeline-face 
-;  '((t (:inherit mode-line)))
-;  "wn-mode modeline face")
+(defface wn-modeline-face
+  '((t ( :inherit mode-line)))
+  "wn-mode modeline face")
+
+(provide 'wn-mode)
+
+;;; wn-mode.el ends here
