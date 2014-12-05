9276178ec509b9f8fb5d6234f4cd23f17d2468e2
Luís Oliveira <luismbo@gmail.com>
2014-12-05 14:22:55 +0000

    Add header, commentary, etc

--- wn-mode.el
+++ wn-mode.el
@@ -1,8 +1,33 @@
-;;; wn-model.el -- numeric window switching shortcuts
-;;; Version: 0.1
-;;; 
+;;; wn-model.el -- numeric window switching shortcuts -*- lexical-binding: t -*-
 
-;;; Window number
+;; This library was found at the Emacs Wiki and is probably in the
+;; public domain.
+
+;; Author: Anonymous
+;; Maintainer: Luís Oliveira <luismbo@gmail.com>
+;; Keywords: buffers windows switching-windows
+
+;;; Commentary:
+
+;; This library defines a global minor mode called `wn-mode' that adds
+;; keyboard shortcuts to quickly switch between visible windows within
+;; the current Emacs frame.
+;;
+;; To activate, simply add
+;;   (wn-mode)
+;; to your `~/.emacs'.
+;;
+;; By default the shortcuts are M-0, M-1, ..., M-9, M-#.
+;;
+;; Customize `wn-keybinding-format' if you wish to use different key
+;; bindings, e.g.:
+;;   (setq wn-keybinding-format "C-c %s")
+;;
+;; Invoke M-x remake-wn-mode-map if you want it to take effect
+;; immediately.
+
+;;; Code:
+
 (defun wn-window-list ()
   "Return a list of ordered windows on the current frame"
   (window-list (selected-frame) 0 (minibuffer-window)))
